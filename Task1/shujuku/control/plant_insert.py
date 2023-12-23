10d479691f30e2c6e91fdd7745123eb8cc60e22b
13708736598Xlh <131777366+13708736598Xlh@users.noreply.github.com>
2023-12-23 20:58:10 +0800

    Add files via upload

---
+++ Task1/shujuku/control/plant_insert.py
@@ -0,0 +1,91 @@
+import Factor
+from classes.classes import classes as classs
+from classes import plant,plant_classes
+FC=Factor.dao_factory()
+def creat_plant(user=None):
+    plants=FC.create("plant")
+    sql ="select species_name from plant"
+    result_name=plants.select(sql)
+    judename = 0
+    name=""
+    while judename==0:
+        name=input("请输入植物名：")
+        for plantname in result_name:
+            judename =1
+            if plantname[0]==name:
+                print("植物名重复，请重新输入：")
+                judename =0
+                break
+
+    print("请从以下科名中输入植物科名（如果要增加，请输入要增加的科名）")
+    sql="select class_id,class_name from classes where rank=0"
+    result=plants.select(sql)
+    for Family in result:
+        print(Family[1],end=' ')
+    print("")
+    Family_name=input("")
+    judge=0
+    Family_id=-1
+    result_genus=[]
+    for Family in result:
+        if Family_name==Family[1]:
+            Family_id=Family[0]
+            judge=1
+            break
+    if judge==1:
+        sql2="select class_id,class_name from classes where parent=%s"%Family_id
+        result_genus=plants.select(sql2)
+    else:
+        classess=FC.create("classes")
+        new_class=classs()
+        new_class.__int__(rank=0,class_name=Family_name)
+        classess.insert(new_class)
+        sql_Family_id="select class_id from classes where class_name='%s'"%Family_name
+        result_famil_id=classess.select(sql_Family_id)
+        Family_id=result_famil_id[0][0]
+    print("请从以下科名中输入植物属名（如果要增加，请输入要增加的属名）")
+    if result_genus:
+        for Genus in result_genus:
+            print(Genus[1],end=' ')
+    print("")
+    Genus_name=input()
+    judge2=0
+    Genus_id=-1
+    for Genuss in result_genus:
+        if Genus_name==Genuss[1]:
+            Genus_id=Genuss[0]
+            judge2=1
+            break
+    if judge2==0:
+        classess = FC.create("classes")
+        new_class = classs()
+        new_class.__int__(rank=1,parent=Family_id, class_name=Genus_name)
+        classess.insert(new_class)
+        sql_Genus_id = "select class_id from classes where class_name='%s'" % Genus_name
+        result_Genus_id = classess.select(sql_Genus_id)
+        Genus_id = result_Genus_id[0][0]
+    alies=input("请输入植物别名：")
+    morphology=input("请输入植物形态特征：")
+    key_tech=input("请输入植物栽培技术")
+    value=input("请输入植物价值")
+    envirment=input("请输入植物的生长环境")
+    new_plant=plant.plant()
+    new_plant.__int__(species_name=name,alies=alies,morphology=morphology,value=value,key_tech=key_tech,environment=envirment)
+    plants.insert(new_plant)
+    sql3="select plant_id from plant where species_name='%s'"%name
+    result_id=plants.select(sql3)
+    FC_plant_classes=FC.create("plant_classes")
+    new_plant_classes=plant_classes.plant_classes()
+    new_plant_classes.__int__(plant_id=result_id[0][0],class_id=Genus_id)
+    FC_plant_classes.insert(new_plant_classes)
+    if judge!=1:
+        view="view"+str(Family_id)
+        print(view)
+        sql="create view %s as select DISTINCT plant.plant_id,species_name,c1.class_name as keming,c2.class_name as shuming,Plant.alia, \
+        plant.key_tech,plant.morphology,plant.value,plant.created_by,plant.created_at, \
+        plant.updated_at from classes c1,classes c2,plant_class,Plant,Pest,plant_pest \
+        where plant.plant_id=plant_class.plant_id and plant_class.class_id=c2.class_id \
+        and c1.class_id=(select c2.parent) and c1.class_id=%d"%(view,Family_id)
+        print(sql)
+        plants.creatview(sql)
+creat_plant()
---
+++ Task1/shujuku/control/region_insert.py
@@ -0,0 +1,100 @@
+import Factor
+from classes import region as Regionn
+from classes import plant,plant_region
+
+FC=Factor.dao_factory()
+def creat_region():
+    plants = FC.create("plant")
+    regions = FC.create("region")
+    plant_regions = FC.create("plant_region")
+    sql = "select plant_id,species_name from plant"
+    result_name = plants.select(sql)
+    judename = 0
+    name = ""
+    print("请输入要添加地区的植物名称：")
+    for plantname in result_name:
+        print(plantname[1], end=" ")
+    print()
+    plant_id = -1
+    while judename == 0:
+        name = input()
+        for plantname in result_name:
+            if plantname[1] == name:
+                judename = 1
+                plant_id = plantname[0]
+                break
+        if judename == 0:
+            print("输入植物名称不存在，请重新输入：")
+    print("请从以下地区中输入植物省级地区（如果要增加，请输入要增加的市级名）")
+    sql="select region_id,region_name from region where rank=0"
+    result=regions.select(sql)
+    for province in result:
+        print(province[1],end=' ')
+    print("")
+    province_name=input("")
+    judge=0
+    province_id=-1
+    result_city=[]
+    for province in result:
+        if province_name==province[1]:
+            province_id=province[0]
+            judge=1
+            break
+    if judge==1:
+        sql2="select region_id,region_name from region where parent=%s"%province_id
+        result_city=regions.select(sql2)
+    else:
+        new_region = Regionn.region()
+        new_region.__int__(rank=0,region_name=province_name)
+        regions.insert(new_region)
+        sql_province_id="select region_id from region where region_name='%s'"%province_name
+        result_province_id=regions.select(sql_province_id)
+        province_id=result_province_id[0][0]
+    print("请从以下市级名中输入植物所在地市级名（如果要增加，请输入要增加的市级名）")
+    if result_city:
+        for city in result_city:
+            print(city[1],end=' ')
+    print("")
+    city_name=input()
+    judge2=0
+    city_id=-1
+    for citys in result_city:
+        if city_name==citys[1]:
+            city_id=citys[0]
+            judge2=1
+            break
+    result_county=[]
+    if judge2==0:
+        new_region = regions()
+        new_region.__int__(rank=1,parent=province_id, region_name=city_name)
+        regions.insert(new_region)
+        sql_city_id = "select region_id,region_name from region where region_name='%s'" % city_name
+        result_city_id = regions.select(sql_city_id)
+        city_id = result_city_id[0][0]
+    else:
+        sql2 = "select region_id,region_name from region where parent=%s" % city_id
+        result_county = regions.select(sql2)
+    print("请从以下县级名中输入植物所在地市级名（如果要增加，请输入要增加的县级名）")
+    if result_county:
+        for county in result_county:
+            print(county[1], end=' ')
+    print("")
+    county_name = input()
+    judge3 = 0
+    county_id = -1
+    for countys in result_county:
+        if county_name == countys[1]:
+            county_id = countys[0]
+            judge3 = 1
+            break
+    if judge3 == 0:
+        new_region = regions()
+        new_region.__int__(rank=2, parent=city_id, region_name=county_name)
+        regions.insert(new_region)
+        sql_county_id = "select region_id,region_name from region where region_name='%s'" % county_name
+        result_county_id = regions.select(sql_county_id)
+        county_id = result_county_id[0][0]
+    new_plant_region=plant_region.plant_region()
+    new_plant_region.__int__(plant_id=plant_id,region_id=county_id)
+    plant_regions.insert(new_plant_region)
+creat_region()
